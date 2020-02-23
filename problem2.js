a8145ff726d69befa13e86e3f97827aa56b3f2da
Esin Saribudak <esin.saribudak@gmail.com>
2020-02-23 16:39:29 -0600

    Create problem2.js

---
+++ problem2.js
@@ -0,0 +1,10 @@
+function evenFibonnacisSum(upperlimit){
+  //your code goes here!
+}
+
+console.log(evenFibonnacisSum(100))
+// should be 44
+// Explanation: The even Fibonnacis below 100 are 2, 8, and 34. Their sum is 44. 
+
+console.log(evenFibonnacisSum(4000000))
+// ?????
