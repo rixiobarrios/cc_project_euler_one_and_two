880a3bf6abe05d3b3428b272cf5c8d36c08d5602
Esin Saribudak <esin.saribudak@gmail.com>
2020-02-23 16:39:11 -0600

    Create problem1.js

---
+++ problem1.js
@@ -0,0 +1,10 @@
+function sumOfMultiplesofThreeAndFive(num) {
+  // your code goes here! 
+}
+
+console.log(sumOfMultiplesofThreeAndFive(10))
+// should be 23
+// Explanation: Multiples of three and five below 10 are 3, 5, 6, and 9. The sum of 3, 5, 6 and 9 is 23. 
+
+console.log(sumOfMultiplesofThreeAndFive(1000))
+// ?????
